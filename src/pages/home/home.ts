5f57ed1e8798e4b6cea30a1bce6dc0dd4defaac5
danditeoa <taci.dandi@gmail.com>
2019-10-30 11:03:10 -0300

    create ionic project

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+import { Carro } from '../../modelos/carro';
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,26 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public carros: Carro[];
 
+  constructor(public navCtrl: NavController,
+              private http: HttpClient,
+              private loading: LoadingController) {
+
+    let load = this.loading.create({
+      content: 'Carregando...'
+    });
+
+    load.present();
+
+    this.http.get<Carro[]>('http://localhost:8080/api/carro/listatodos')
+                .subscribe(
+                  (carros) => {
+                    this.carros = carros;
+
+                    load.dismiss();
+                  }
+                );
   }
 
 }
